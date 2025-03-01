075437be8c1068d1c5300d6c80825cbf8daeef07
God_Chippy_V2 <sebastiansaldana085@gmail.com>
2025-02-28 19:36:32 -0600

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Jacob":return img`
+. . . . 3 3 3 3 . . . . . 
+. . 3 3 3 3 3 3 3 3 . . . 
+. 3 3 3 3 3 3 a 3 3 3 . . 
+3 3 3 3 3 3 a a 3 3 3 a . 
+3 3 3 a 3 3 3 3 3 3 3 a . 
+a a a 3 3 3 d d 3 3 a a . 
+3 3 3 3 3 d d 3 3 a a 3 . 
+3 3 3 b f d d f b 3 3 3 . 
+. f d 1 f d d f 1 d f . . 
+. f d d d d d d d d f . . 
+. f f f d d d d f f f . . 
+f d f b 8 8 8 8 b f d f . 
+d d f 8 8 8 8 8 8 f d d . 
+d d f 6 6 6 6 6 6 f d d . 
+f f . 6 6 6 6 6 6 . f f . 
+. . . f f . . f f . . . . 
+`;
+            case "image2":
+            case "Time Space Ship":return img`
+....ffffff..............
+....ff33ccf.........cf..
+.....ffccccfff.....caf..
+....cc33333333cccccaaf..
+...c9b3aaa33333cccaa3f..
+..c999aa3333333333a3fc..
+.c3b9993333333333333fcc.
+c333b1111b33333333cc33cf
+f3333311993333ccccccc33f
+.f33333333333c333cffffff
+..ff3333333caa3333ff....
+....fffffffffaa3333fc...
+.........f3cffaa3333c...
+.........fccfffc3333c...
+..........fc3ffffffff...
+...........c3fff........
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
